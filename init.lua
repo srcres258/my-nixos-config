2b4b3ee35c5b05d90e6106efa8488d3981bc3459
src_resources <src.res.211@gmail.com>
2025-11-28 20:13:40 +0800

    Modify a shortcut key

--- init.lua
+++ init.lua
@@ -382,10 +382,8 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 
-vim.keymap.set({ "n", "x" }, "<S-H>", "^", { desc = "Start of line" })
-vim.keymap.set({ "n", "x" }, "<S-L>", "$", { desc = "End of line" })
-vim.keymap.set("n", "y<S-H>", "y^", { desc = "Yank from start of line" })
-vim.keymap.set("n", "y<S-L>", "y$", { desc = "Yank to end of line" })
+vim.keymap.set({ "n", "x", "o" }, "<S-H>", "^", { desc = "Start of line" })
+vim.keymap.set({ "n", "x", "o" }, "<S-L>", "$", { desc = "End of line" })
 
 vim.keymap.set({ "n", "x" }, "Q", "<CMD>:qa<CR>")
 vim.keymap.set({ "n", "x" }, "qq", "<CMD>:q<CR>")
