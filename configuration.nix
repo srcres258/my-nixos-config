d5383e3db48500789f9918b49681a60e18dcebdd
src_resources <src.res.211@gmail.com>
2025-12-02 20:55:32 +0800

    Add postgresql

--- configuration.nix
+++ configuration.nix
@@ -310,6 +310,8 @@
 
   services.gvfs.enable = true;
 
+  services.postgresql.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
