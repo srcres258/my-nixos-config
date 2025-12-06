6f570ac3d1bf43dffadb7ac93148128f3669afc9
src_resources <src.res.211@gmail.com>
2025-12-06 10:02:35 +0800

    Fix fish prompt

--- home/develop.nix
+++ home/develop.nix
@@ -10,28 +10,29 @@
   programs.fish = {
     enable = true;
 
-    prompt = ''
-      function fish_prompt
-        set -l last_status $status
-        set -l cyan  (set_color cyan)
-        set -l yellow (set_color yellow)
-        set -l red    (set_color red)
-        set -l blue   (set_color blue)
-        set -l green  (set_color green)
-        set -l normal (set_color normal)
+    functions = {
+      fish_prompt = ''
+        # 示例：简单用户@主机 + 当前目录 + 状态指示符
+        function fish_prompt
+          set -l last_status $status
+          set -l cyan (set_color cyan)
+          set -l yellow (set_color yellow)
+          set -l red (set_color red)
+          set -l green (set_color green)
+          set -l normal (set_color normal)
 
-        # 第一行：当前目录 + git 信息
-        echo -n "$cyan$PWD$normal"
-        __fish_git_prompt " %s"
+          # 显示当前目录
+          echo -n "$cyan$PWD$normal"
 
-        # 第二行：如果上个命令失败就显示红色 >
-        if test $last_status -ne 0
-          echo -n "$red>$normal "
-        else
-          echo -n "$green>$normal "
+          # 如果上个命令失败，显示红色提示符
+          if test $last_status -ne 0
+            echo "$red❯$normal "
+          else
+            echo "$green❯$normal "
+          end
         end
-      end
-    '';
+      '';
+    };
   };
 }
 
