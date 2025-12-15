ea3410c49ef50aac720be83b64ef173ce4ad61e8
src_resources <src.res.211@gmail.com>
2025-12-15 11:00:37 +0800

    Set console font for laptop

--- devices/srcres-laptop/configuration.nix
+++ devices/srcres-laptop/configuration.nix
@@ -54,6 +54,8 @@
         package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
+    console.font = "ter-128b";
+
     # This option defines the first version of NixOS you have installed on this particular machine,
     # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
     #
