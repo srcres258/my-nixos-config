9fc5e58348f54452cd18cd75ffe6309c490fadb2
src_resources <src.res.211@gmail.com>
2025-12-19 21:19:23 +0800

    Add 8080 tcp port in firewall

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,7 @@
     networking.networkmanager.enable = true;
 # Open some ports for testing purposes.
     networking.firewall = {
-        allowedTCPPorts = [ 11451 51413 ];
+        allowedTCPPorts = [ 8080 11451 51413 ];
         allowedUDPPorts = [ 51413 ];
     };
 
