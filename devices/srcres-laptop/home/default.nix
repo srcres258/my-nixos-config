d2c80799263c37c3cf2a86a71403027af5cff1e3
src_resources <src.res.211@gmail.com>
2025-12-26 19:47:24 +0800

    Disable wallpaper for laptop

--- devices/srcres-laptop/home/default.nix
+++ devices/srcres-laptop/home/default.nix
@@ -5,26 +5,26 @@
     inputs,
     ...
 }: {
-    systemd.user.services."mpvpaper" = let
-        wallpaperSrc = ./wallpapers/bg.mp4;
-        mpvOptions = [
-            "loop=inf no-audio hwdec=vaapi"
-        ];
-    in {
-        Unit = {
-            Description = "mpvpaper dynamic wallpaper";
-        };
-
-        Service = {
-            Type = "simple";
-            ExecStart = "${pkgs.mpvpaper}/bin/mpvpaper -o '${builtins.concatStringsSep " " mpvOptions}' '*' ${wallpaperSrc}";
-            Restart = "on-failure";
-            RestartSec = 2;
-        };
-
-        Install = {
-            WantedBy = [ "graphical-session.target" ];
-        };
-    };
+    # systemd.user.services."mpvpaper" = let
+    #     wallpaperSrc = ./wallpapers/bg.mp4;
+    #     mpvOptions = [
+    #         "loop=inf no-audio hwdec=vaapi"
+    #     ];
+    # in {
+    #     Unit = {
+    #         Description = "mpvpaper dynamic wallpaper";
+    #     };
+    #
+    #     Service = {
+    #         Type = "simple";
+    #         ExecStart = "${pkgs.mpvpaper}/bin/mpvpaper -o '${builtins.concatStringsSep " " mpvOptions}' '*' ${wallpaperSrc}";
+    #         Restart = "on-failure";
+    #         RestartSec = 2;
+    #     };
+    #
+    #     Install = {
+    #         WantedBy = [ "graphical-session.target" ];
+    #     };
+    # };
 }
 
