ff328a820e04b182356b2aaddcaa1032c72e0da5
src_resources <src.res.211@gmail.com>
2026-01-10 10:33:35 +0800

    Enable vim feature inside fish

--- configuration.nix
+++ configuration.nix
@@ -162,7 +162,18 @@
         rm -r "$out/share/man/man3"
     '';
 
-    programs.fish.enable = true;
+    programs.fish = {
+        enable = true;
+
+        interactiveShellInit = ''
+            fish_vi_key_bindings insert
+
+            set fish_cursor_default     block      blink
+            set fish_cursor_insert      line       blink
+            set fish_cursor_replace_one underscore blink
+            set fish_cursor_visual      block
+        '';
+    };
 
     programs.niri.enable = true;
 
