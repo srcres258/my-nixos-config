d48492441c4cdad7fc22e2a82f0af08512c15bb1
srcres258 <src.res.211@gmail.com>
2026-04-13 09:02:34 +0800

    Adjust home state version settings
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- devices/srcres-laptop/home/default.nix
+++ devices/srcres-laptop/home/default.nix
@@ -27,5 +27,10 @@
   #         PartOf = [ "graphical-session.target" ];
   #     };
   # };
+
+  # !IMPORTANT!
+  # This option should NOT be changed, except for installation
+  # for a completely new machine or a new user.
+  home.stateVersion = "25.05";
 }
 
--- home/pure/default.nix
+++ home/pure/default.nix
@@ -348,10 +348,5 @@ in
       ];
     };
   };
-
-  # !IMPORTANT!
-  # This option should NOT be changed, except for installation
-  # for a completely new machine or a new user.
-  home.stateVersion = "25.05";
 }
 
