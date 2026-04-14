b4aff5c9816c907f323018ec717cdfc78de8670f
srcres258 <src.res.211@gmail.com>
2026-04-14 12:51:28 +0800

    Fix indent
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- devices/srcres-orange-pi/home/default.nix
+++ devices/srcres-orange-pi/home/default.nix
@@ -19,21 +19,21 @@ in {
   home.packages = with pkgs; [
       swaybg
   ];
-  
+
   systemd.user.services."swaybg" = let
       backgroundImg = "/home/${username}/background.jpg";
   in {
       Unit = {
           Description = "Sway static wallpaper background service";
       };
-  
+
       Service = {
           Type = "simple";
           ExecStart = "${pkgs.swaybg}/bin/swaybg --mode full --image ${backgroundImg}";
           Restart = "on-failure";
           RestartSec = 2;
       };
-  
+
       Install = {
           WantedBy = [ "graphical-session.target" ];
           PartOf = [ "graphical-session.target" ];
