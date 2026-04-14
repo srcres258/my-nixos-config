4ff53d2490fd1bfec1cf536ebd5def822a036243
srcres258 <src.res.211@gmail.com>
2026-04-14 12:47:11 +0800

    Add swaybg and wallpaper
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- devices/srcres-orange-pi/home/default.nix
+++ devices/srcres-orange-pi/home/default.nix
@@ -1,6 +1,10 @@
-{ ... }: {
-  home.username = "srcres";
-  home.homeDirectory = "/home/srcres";
+{ pkgs
+, ...
+}: let
+  username = "srcres";
+in {
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
   home.stateVersion = "25.11";
 
   # Orange Pi keeps desktop parity without video wallpaper service.
@@ -10,5 +14,29 @@
   programs.kitty.settings = {
     enable_audio_bell = false;
     remember_window_size = false;
+  };
+
+  home.packages = with pkgs; [
+      swaybg
+  ];
+  
+  systemd.user.services."swaybg" = let
+      backgroundImg = "/home/${username}/background.jpg";
+  in {
+      Unit = {
+          Description = "Sway static wallpaper background service";
+      };
+  
+      Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.swaybg}/bin/swaybg --mode full --image ${backgroundImg}";
+          Restart = "on-failure";
+          RestartSec = 2;
+      };
+  
+      Install = {
+          WantedBy = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+      };
   };
 }
