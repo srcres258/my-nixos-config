13db585ec52918e9db3db1a36b70bc382977aa99
srcres258 <src.res.211@gmail.com>
2026-04-16 08:49:15 +0800

    Show command input in Neovim
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- home/pure/init.lua
+++ home/pure/init.lua
@@ -239,6 +239,9 @@ require('noice').setup({
     routes = {
         { filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } },
         { filter = { event = "msg_show", kind = "" }, opts = { skip = true } }
+    },
+    cmdline = {
+        enabled = true,
     }
 })
 
@@ -722,6 +725,8 @@ vim.o.foldenable = true
 vim.o.foldcolumn = "0" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
+vim.o.showcmd = true
+vim.o.cmdheight = 1
 vim.opt.fillchars = {
     fold = " ",
     foldopen = "▾",
