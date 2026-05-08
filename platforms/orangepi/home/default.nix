7de8ffea88b624296211141e89427c0e1ed373ae
srcres258 <src.res.211@gmail.com>
2026-05-08 22:13:47 +0800

    Add screenshot tools for Orange Pi
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- platforms/orangepi/home/default.nix
+++ platforms/orangepi/home/default.nix
@@ -8,5 +8,12 @@
     ./vscode.nix
   ];
 
+  home.packages = with pkgs; [
+    # Screenshot tools
+    grim
+    slurp
+    satty
+  ];
+
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
 }
