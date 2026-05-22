a7242f219398f158714e4abdb39d10ae1d19f235
srcres258 <src.res.211@gmail.com>
2026-05-22 22:23:42 +0800

    Add nixpkgs overlay for personal NUR repo
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- home/default.nix
+++ home/default.nix
@@ -5,5 +5,21 @@
 , ...
 }: {
   imports = [ ./pure ./system ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      nur = import inputs.nur {
+        pkgs = prev;
+        repoOverrides = let
+          username = "srcres258";
+          remoteUrl = "https://github.com/${username}/nur-packages/archive/main.tar.gz";
+        in {
+          srcres258 = import (builtins.fetchTarball remoteUrl) {
+            pkgs = prev;
+          };
+        };
+      };
+    })
+  ];
 }
 
