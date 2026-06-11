8e0d9f576753dd5d562f5514105de328c015cf1d
srcres258 <src.res.211@gmail.com>
2026-06-11 23:17:08 +0800

    Add more config for codex
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- home/pure/codex.nix
+++ home/pure/codex.nix
@@ -1,4 +1,30 @@
 { ... }: {
-  programs.codex.enable = true;
+  programs.codex = {
+    enable = true;
+
+    settings = {
+      model_provider = "micu";
+      model = "gpt-5.3-codex";
+      model_reasoning_effort = "high";
+      disable_response_storage = true;
+
+      model_providers = {
+        micu = {
+          name = "micu";
+          base_url = "https://www.micuapi.ai/v1";
+          wire_api = "responses";
+          requires_openai_auth = true;
+          model_context_window = 1000000;
+          model_auto_compact_token_limit = 900000;
+        };
+      };
+
+      mcp_servers = {
+        pywellen = {
+          command = "pywellen-mcp";
+        };
+      };
+    };
+  };
 }
 
