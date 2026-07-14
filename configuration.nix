e66900322a8da98aa5564ce413fb33a4516a2497
srcres258 <src.res.211@gmail.com>
2026-07-14 23:21:27 +0800

    Adjust the order of substituters
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -320,8 +320,8 @@
   nix.channel.enable = false;
 
   nix.settings.substituters = [
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://mirror.sjtu.edu.cn/nix-channels/store"
   ];
 
   # Cachix: yazi
