b9716fe483377f3073eb2564e2407d87fd196274
srcres258 <src.res.211@gmail.com>
2026-07-16 15:39:20 +0800

    Disable a substituter
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -322,7 +322,7 @@
   nix.settings.substituters = [
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirrors.ustc.edu.cn/nix-channels/store"
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
+    #"https://mirror.sjtu.edu.cn/nix-channels/store"
   ];
 
   # Cachix: yazi
