116fbb2fccdefe2b442d3846b9c9912c9ef1d60a
srcres258 <src.res.211@gmail.com>
2026-08-29 18:53:42 +0800

    Remove general SSH settings
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -223,17 +223,6 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      X11Forwarding = true;
-      PermitRootLogin = "yes";
-      PasswordAuthentication = true;
-    };
-    openFirewall = true;
-  };
-
   fonts = {
     packages = with pkgs; [
       adwaita-fonts
