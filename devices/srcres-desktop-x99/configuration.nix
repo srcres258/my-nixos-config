3ca0be408829d0ef56cb42b0444cb42ee93cfbd5
srcres258 <src.res.211@gmail.com>
2026-08-29 18:54:09 +0800

    Add SSH settings for x99 device
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- devices/srcres-desktop-x99/configuration.nix
+++ devices/srcres-desktop-x99/configuration.nix
@@ -40,6 +40,12 @@
 
   hardware.amdgpu.opencl.enable = true;
 
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
